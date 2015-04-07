28fa779c516c18b793fd69e2ecaf8a5d4032e7ee
zashii5793 <hideki.ishizashi@gmail.com>
2015-04-07 10:28:06 +0000

    initialize repository

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :entry
-  validates :name, lentgth:{ maximum: 15}
-  validates :body, presence:true, lentgth:{ maximum: 140}
+  validates :name, length:{ maximum: 15}
+  validates :body, presence:true, length:{ maximum: 140}
   
 end
